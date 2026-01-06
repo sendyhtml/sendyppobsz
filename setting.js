e464645c88013fb009eee16ecc482c2cd1a559fc
sendyhtml <sendyfalisha@gmail.com>
2026-01-06 18:16:51 +0700

    Add setting.js

---
+++ setting.js
@@ -0,0 +1,4 @@
+global.feenya = 500 //atur fee nya misal harga asli 1.480 + fee 200 jadi harganya 1.680
+global.apikey = "sk-kbbvcmwqi8er7n" //isi apikey dari https://ciaatopup.my.id minta verifikasi h2h ke admin nya
+
+
